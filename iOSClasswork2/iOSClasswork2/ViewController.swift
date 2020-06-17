65906a459d0663b01c2afbbfa4108ec8eae0db6c
lujain <63684917+lujainalkandari@users.noreply.github.com>
2020-06-17 23:26:23 +0300

    a7san classwork
    hi

--- iOSClasswork2/iOSClasswork2/ViewController.swift
+++ iOSClasswork2/iOSClasswork2/ViewController.swift
@@ -16,13 +16,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var animalEmojiLabel: UILabel!
     
     
-    @IBAction func AddAnimalToArray(_ sender: Any) {
+    @IBAction func AddAnimalToArray(cow sender: Any) {
         let animal = animalTextField.text!
         
         
         // MARK: - اكتب ال if statement الكود تحت
-        
-        
+        if animal == "cat" {
+            emojiArray.append(  "🐱")
+        }
+        if animal == "mouse" {
+            emojiArray.append( "🐭")
+        }
+        if animal == "panda"{
+        emojiArray.append ("🐼")
         // MARK: - لا تقم بتغيير هذا السطر⚠️⚠️
         animalTextField.text = ""
     }
@@ -30,7 +36,7 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func ShowAnimal(_ sender: Any) {
+        func ShowAnimal(cat_ sender: Any) {
         // MARK:  تقوم هذه الدالة بعرض الإيموجيز للحيوانات في الأسفل باستعمال `for in`
         animalEmojiLabel.text = ""
         for label in emojiArray
@@ -38,7 +44,8 @@ class ViewController: UIViewController {
             animalEmojiLabel.text! += label
         }
         /// **BONUS**: 🎁 قم بمسح مكونات المصفوفة بعد عرضها
-
+            emojiArray.removeAll()
     }
 }
 
+}
---
+++ wednesdays-CW/ios-cw1-p1.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+let family = ["lujain", "Ahmad", "Hamad", "lamar" , "layan"]
+var even = [ 2, 4, 6, 8, 10, 12]
+var grades = [88, 90, 60]
+var TrueFalse = [true, true, true,false]
+print(family[0], family[4])
+print(even [0],evens[4])
+print(grades[0], grades[2])
+print(trueFalse[0], trueFalse[3])
+
+
+
